35f55d17f3ca62e07181639958d62e68e4ad3b14
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2021-03-21 20:42:00 -0500

    Add defense.py

---
+++ defense.py
@@ -0,0 +1,36 @@
+import subprocess
+from subprocess import check_output
+from win10toast import ToastNotifier
+import time
+
+toast = ToastNotifier()
+
+def get_initial_mac_gw():
+    init_mac_gw = ""
+    ip_gw = "192.168.1.1"
+    line_mac = check_output(['arp','-a', ip_gw])
+    #line_mac = subprocess.run(['arp','-a', ip_gw], stdout=subprocess.PIPE)
+    #line_mac = subprocess.run(['arp',ip_gw], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return line_mac
+
+
+def service_anti_poisoning(init_mac_gw):
+    print("Service Anti-ARP-Poisoning")
+    ip_gw = "192.168.1.1"
+    while True:
+        current_mac_gw = check_output(['arp','-a', ip_gw])
+        #current_mac_gw = subprocess.run(['arp',ip_gw], stdout=subprocess.PIPE).stdout.decode('utf-8') 
+        if init_mac_gw != current_mac_gw:
+            print("Health status: Attack Detected!!")
+            toast.show_toast("Amenaza detectada","Es posible que este bajo un ataque de red", duration=10)
+        else:
+            print("Health Status: Good!!")
+        time.sleep(5)
+
+def main():
+    init_mac_gw = get_initial_mac_gw()
+    #print(init_mac_gw)
+    service_anti_poisoning(init_mac_gw)
+
+## Calling main function ##
+main()
